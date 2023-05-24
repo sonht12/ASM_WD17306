0d9c83c58c9e1835b358fec7911494dbfca472db
Sonht21111 <son0379878379@gmail.com>
2023-05-24 16:15:12 +0700

    khởi tạo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/Client/home-page/home-page.component';
+import { ProductPageComponent } from './components/Client/product-page/product-page.component';
+import { ProductDetailComponent } from './components/Client/product-detail/product-detail.component';
+import { ProductAdminComponent } from './components/Admin/Product/product-admin/product-admin.component';
+import { ProductAddComponent } from './components/Admin/Product/product-add/product-add.component';
+import { ProductEditComponent } from './components/Admin/Product/product-edit/product-edit.component';
+import { ListCategoryComponent } from './components/Admin/Category/list-category/list-category.component';
+import { CategoryAddComponent } from './components/Admin/Category/category-add/category-add.component';
+import { CategoryEditComponent } from './components/Admin/Category/category-edit/category-edit.component';
+import { ListUserComponent } from './components/Admin/user/list-user/list-user.component';
+import { UserAddComponent } from './components/Admin/user/user-add/user-add.component';
+import { UserEditComponent } from './components/Admin/user/user-edit/user-edit.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'product', component: ProductPageComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+
+  {
+    path: 'admin', children:[
+      // product
+      {path: 'product', component: ProductAdminComponent},
+      {path: 'product/add', component: ProductAddComponent},
+      {path: 'product/edit', component: ProductEditComponent},
+      // category
+      {path: 'category', component: ListCategoryComponent},
+      {path: 'category/add', component: CategoryAddComponent},
+      {path: 'category/edit', component: CategoryEditComponent},
+      // user
+      {path: 'user', component: ListUserComponent},
+      {path: 'user/add', component: UserAddComponent},
+      {path: 'user/edit', component: UserEditComponent},
+
+    ]
+  },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductAdminComponent } from './components/Admin/Product/product-admin/product-admin.component';
+import { ProductAddComponent } from './components/Admin/Product/product-add/product-add.component';
+import { ProductEditComponent } from './components/Admin/Product/product-edit/product-edit.component';
+import { ListCategoryComponent } from './components/Admin/Category/list-category/list-category.component';
+import { CategoryAddComponent } from './components/Admin/Category/category-add/category-add.component';
+import { CategoryEditComponent } from './components/Admin/Category/category-edit/category-edit.component';
+import { ListUserComponent } from './components/Admin/user/list-user/list-user.component';
+import { UserAddComponent } from './components/Admin/user/user-add/user-add.component';
+import { UserEditComponent } from './components/Admin/user/user-edit/user-edit.component';
+import { HomePageComponent } from './components/Client/home-page/home-page.component';
+import { ProductPageComponent } from './components/Client/product-page/product-page.component';
+import { ProductDetailComponent } from './components/Client/product-detail/product-detail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductAdminComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ListCategoryComponent,
+    CategoryAddComponent,
+    CategoryEditComponent,
+    ListUserComponent,
+    UserAddComponent,
+    UserEditComponent,
+    HomePageComponent,
+    ProductPageComponent,
+    ProductDetailComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
