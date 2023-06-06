17f814e0fe72153294ed32b78e139ac0d85543f9
sonht12 <son0379878379@gmail.com>
2023-06-06 21:27:49 +0700

    Back_End

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import router from "../Router/product";
+import user from "../Router/user";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import CategoryRoute from "../Router/category";
+import productRouter from '../Router/product'
+dotenv.config();
+const app = express();
+
+const { API } = process.env;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", router);
+app.use("/api", user);
+app.use("/api", CategoryRoute);
+app.use('/api', productRouter)
+
+mongoose.connect(API);
+
+export const viteNodeApp = app;
