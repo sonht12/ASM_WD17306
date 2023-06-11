e9ea038a41bbf33610469b2958358b64ba1a49d5
school2003 <117899070+ngoductruong@users.noreply.github.com>
2023-06-11 20:17:56 +0700

    feat/show+delete_user(front-end)-refs-AA-30

--- src/app/components/Admin/user/list-user/list-user.component.ts
+++ src/app/components/Admin/user/list-user/list-user.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { IUser } from 'src/app/interface/user';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent {
-
+  users!: IUser[];
+  constructor(private abc:AuthService){
+    this.abc.getUsers().subscribe(data=>{
+      this.users=data
+      console.log(this.users);
+      
+    })
+  }
+  xoauser(id:any){
+    this.abc.deleteUser(id).subscribe(()=> {
+      this.users=this.users.filter(item=>item._id !==id)
+    })
+  }
 }
---
+++ src/app/interface/user.ts
@@ -0,0 +1,8 @@
+export interface IUser{
+    _id?: number;
+    name: string;
+    email?: string;
+    password?: string;
+    role?:string
+  }
+  
